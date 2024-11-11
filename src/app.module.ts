a880cc4444f1f97d6beb06fc3ef45743d2aa82a0
shpokas <s.shpokas@gmail.com>
2024-11-11 18:00:16 +0200

    fix: issue with app.modules, server is running

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { cacheConfig } from './config/cache.config';
+import { SupabaseModule } from './supabase/supabase.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, cacheConfig],
+  imports: [AuthModule, UsersModule, cacheConfig, SupabaseModule],
 })
 export class AppModule {}
